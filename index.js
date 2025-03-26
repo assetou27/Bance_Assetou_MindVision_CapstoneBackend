5f5b53e851c0063dd80f0c69e66355454a5ae52c
assetou27 <banceasset@gmail.com>
2025-03-26 15:08:21 -0400

    inde

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ app.get('/api/test', (req, res) => {
 //   });
 // }
 
+if (process.env.NODE_ENV === 'production' && fs.existsSync(path.join(__dirname, 'client/build'))) {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 // Define port
 const PORT = process.env.PORT || 5000;
 
